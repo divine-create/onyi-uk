cec446faabdf84978703bdad292332fe10ffdb7f
Onyiobazi <yireagroup@gmail.com>
2026-08-27 18:48:26 +0100

    Add new images to the gallery with love quotes

--- components/MemoryGallery.tsx
+++ components/MemoryGallery.tsx
@@ -6,12 +6,18 @@ import onyiUkImage from '../together/onyi-uk.jpeg';
 import img1 from '../together/670912389_18040391969716587_5274454571070615870_n.jpg';
 import img2 from '../together/735909159_1036535908792356_4749825635568732570_n.jpg';
 import img3 from '../together/Screenshot 2026-08-27 183442.png';
+import img4 from '../together/485981732_671262061986411_6746469446143006103_n.jpg';
+import img5 from '../together/583330869_854366783675937_8831016272724064080_n.jpg';
+import img6 from '../together/669598104_967479532364661_2575627374349437755_n.jpg';
 
 const memories: Memory[] = [
   { id: '1', title: 'Endless Devotion', description: '"In all the world, there is no heart for me like yours. In all the world, there is no love for you like mine."', imageUrl: onyiUkImage, date: 'Recent' },
   { id: '2', title: 'Soulmates', description: '"I look at you and see the rest of my life in front of my eyes."', imageUrl: img1, date: 'Always' },
   { id: '3', title: 'My Everything', description: '"If I know what love is, it is because of you."', imageUrl: img2, date: 'Always' },
-  { id: '4', title: 'Forever Yours', description: '"I love you not only for what you are, but for what I am when I am with you."', imageUrl: img3, date: 'Always' }
+  { id: '4', title: 'Forever Yours', description: '"I love you not only for what you are, but for what I am when I am with you."', imageUrl: img3, date: 'Always' },
+  { id: '5', title: 'True Love', description: '"You are my today and all of my tomorrows."', imageUrl: img4, date: 'Always' },
+  { id: '6', title: 'My Sunshine', description: '"To the world you may be one person, but to one person you are the world."', imageUrl: img5, date: 'Always' },
+  { id: '7', title: 'Infinite Affection', description: '"I swear I couldn\'t love you more than I do right now, and yet I know I will tomorrow."', imageUrl: img6, date: 'Always' }
 ];
 
 const MemoryGallery: React.FC = () => {
